811e74418939984851cabeea475562659f489821
graperu <dinhphan0511@gmail.com>
2026-06-23 18:50:04 +0700

    Revert "fix: use MySQL-compatible return details column"
    This reverts commit cafdcea53277e98e416f8776443ae69ad462c1a9.

--- backend/src/main/java/com/bookstore/entity/Order.java
+++ backend/src/main/java/com/bookstore/entity/Order.java
@@ -82,7 +82,7 @@ public class Order {
     @Column(name = "return_bank", length = 255)
     private String returnBank;
 
-    @Column(name = "return_details_text", columnDefinition = "TEXT")
+    @Column(name = "return_details_text", columnDefinition = "NVARCHAR(MAX)")
     private String returnDetails;
 
     @Column(nullable = false)
